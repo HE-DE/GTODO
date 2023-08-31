caa251138e65faf7eead565eac809683b627227d
HE-DE <3029511904@qq.com>
2023-08-31 22:18:17 +0800

    feat(vue-f): [+]登录页|引入路由|引入windicss

---
+++ vue-f/src/env.d.ts
@@ -0,0 +1,6 @@
+import VueRouter, { Route } from 'vue-router';
+declare module 'vue/types/vue' {
+  interface Vue {
+    $router: VueRouter
+  }
+}
---
+++ vue-f/src/pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-row class="min-h-screen">
+        <el-col :span="16" class="bg-indigo-500 flex items-center justify-center flex-col">
+            <div>
+                <div class="font-bold text-7xl text-light-50 mb-4">GTODO</div>
+                <div class="text-gray-200 text-xl">一个使用Go+Vue+Vite开发的备忘录</div>
+            </div>
+        </el-col>
+        <el-col :span="8" class="bg-indigo-50 flex items-center justify-center flex-col">
+            <h2 class="font-bold text-5xl text-gray-800 ">欢迎回来</h2>
+            <div class="flex items-center justify-center my-6 text-gray-400 space-x-3">
+                <span class="h-[1px] w-16 bg-gray-400"></span>
+                <span>账号密码登录</span>
+                <span class="h-[1px] w-16 bg-gray-400"></span>
+            </div>
+            <el-form :model="form" class="w-[250px]">
+                <el-form-item>
+                    <el-input v-model="form.username" placeholder="请输入用户名"/>
+                </el-form-item>
+                <el-form-item>
+                    <el-input v-model="form.password" placeholder="请输入密码"/>
+                </el-form-item>
+                <el-form-item>
+                    <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit">登录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+   username: "",
+   password: ""
+})
+
+const onSubmit = () => {
+    console.log('submit!')
+}
+</script>
